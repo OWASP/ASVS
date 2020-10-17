63174a8aa198581e7ca244198c1b7efbeaa54234
Josh Grossman <tghosth@users.noreply.github.com>
2020-10-18 01:49:46 +0300

    Update generate-all.sh

--- 4.0/generate-all.sh
+++ 4.0/generate-all.sh
@@ -4,7 +4,6 @@ lang="en"
 vers="4.0"
 verslong="./docs_$lang/OWASP Application Security Verification Standard $vers-$lang"
 
-
 python3 export.py --format json --language $lang > "$verslong.json"
 python3 export.py --format xml --language $lang > "$verslong.xml"
 python3 export.py --format csv --language $lang > "$verslong.csv"
