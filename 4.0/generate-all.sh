33a881f94b6ce21d1038c7d3a76ddbbec5c855d1
Josh Grossman <tghosth@users.noreply.github.com>
2021-02-18 16:26:10 +0200

    Add a new flat JSON format to the document generation script (#904)

--- 4.0/generate-all.sh
+++ 4.0/generate-all.sh
@@ -5,6 +5,7 @@ vers="4.0"
 verslong="./docs_$lang/OWASP Application Security Verification Standard $vers-$lang"
 
 python3 export.py --format json --language $lang > "$verslong.json"
+python3 export.py --format json_flat --language $lang > "$verslong.flat.json"
 python3 export.py --format xml --language $lang > "$verslong.xml"
 python3 export.py --format csv --language $lang > "$verslong.csv"
 
