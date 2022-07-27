7d523d5253bf3db40cdda7381e67e32358ef0089
tghosth <janda26@hotmail.com>
2022-07-27 12:01:19 +0300

    Include old export functionality in 5.0 folder

--- 5.0/generate-all.sh
+++ 5.0/generate-all.sh
@@ -1,12 +1,25 @@
 #!/bin/bash 
 
 lang="en"
-vers="4.0"
+
+if [ ! -z "$1" ]
+then
+	lang="$1"
+fi
+
+vers="4.0.edge"
 verslong="./docs_$lang/OWASP Application Security Verification Standard $vers-$lang"
 
-python3 export.py --format json --language $lang > "$verslong.json"
-python3 export.py --format json_flat --language $lang > "$verslong.flat.json"
-python3 export.py --format xml --language $lang > "$verslong.xml"
-python3 export.py --format csv --language $lang > "$verslong.csv"
+python3 ./tools/export.py --format json --language $lang > "$verslong.json"
+python3 ./tools/export.py --format json --language $lang --verify-only true
 
-./generate_document.sh $lang $vers
+python3 ./tools/export.py --format json_flat --language $lang > "$verslong.flat.json"
+python3 ./tools/export.py --format json_flat --language $lang --verify-only true
+
+python3 ./tools/export.py --format xml --language $lang > "$verslong.xml"
+python3 ./tools/export.py --format xml --language $lang --verify-only true
+
+python3 ./tools/export.py --format csv --language $lang > "$verslong.csv"
+python3 ./tools/export.py --format csv --language $lang --verify-only true
+
+./tools/generate_document.sh $lang $vers
