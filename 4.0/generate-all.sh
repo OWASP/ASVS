9f565001a984d465e08602c2355f3aca0b194434
ike <isaac.lewis@signfracturecare.org>
2023-04-18 10:40:18 -0700

    4.0 generation

--- 4.0/generate-all.sh
+++ 4.0/generate-all.sh
@@ -10,16 +10,16 @@ fi
 vers="4.0.3"
 verslong="./docs_$lang/OWASP Application Security Verification Standard $vers-$lang"
 
-python3 export.py --format json --language $lang > "$verslong.json"
-python3 export.py --format json --language $lang --verify-only true
+python3 tools/export.py --format json --language $lang > "$verslong.json"
+python3 tools/export.py --format json --language $lang --verify-only true
 
-python3 export.py --format json_flat --language $lang > "$verslong.flat.json"
-python3 export.py --format json_flat --language $lang --verify-only true
+python3 tools/export.py --format json_flat --language $lang > "$verslong.flat.json"
+python3 tools/export.py --format json_flat --language $lang --verify-only true
 
-python3 export.py --format xml --language $lang > "$verslong.xml"
-python3 export.py --format xml --language $lang --verify-only true
+python3 tools/export.py --format xml --language $lang > "$verslong.xml"
+python3 tools/export.py --format xml --language $lang --verify-only true
 
-python3 export.py --format csv --language $lang > "$verslong.csv"
-python3 export.py --format csv --language $lang --verify-only true
+python3 tools/export.py --format csv --language $lang > "$verslong.csv"
+python3 tools/export.py --format csv --language $lang --verify-only true
 
 ./generate_document.sh $lang $vers
