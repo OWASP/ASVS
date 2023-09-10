aeba35abf19733a2d20c74d55a7b8f22ebf08b88
tghosth <janda26@hotmail.com>
2023-09-10 17:01:37 +0300

    Fix v5 export

---
+++ 5.0/generate-all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+ALLOWED_LANGS='en'
+
+echo $@
+
+if [[ -n $@ ]]; then
+  LANGS=$@
+else
+  LANGS=${ALLOWED_LANGS}
+fi
+
+for lang in ${LANGS}; do
+  if [[ " $ALLOWED_LANGS " =~ " $lang " ]]; then
+
+    vers="5.0"
+    verslong="./docs_$lang/OWASP Application Security Verification Standard $vers-$lang"
+
+    python3 tools/export.py --format json --language $lang > "$verslong.json"
+    python3 tools/export.py --format cdx_json --language $lang > "$verslong.cdx.json"
+    python3 tools/export.py --format json --language $lang --verify-only true
+
+    python3 tools/export.py --format json_flat --language $lang > "$verslong.flat.json"
+    python3 tools/export.py --format json_flat --language $lang --verify-only true
+
+    python3 tools/export.py --format xml --language $lang > "$verslong.xml"
+    python3 tools/export.py --format xml --language $lang --verify-only true
+
+    python3 tools/export.py --format csv --language $lang > "$verslong.csv"
+    python3 tools/export.py --format csv --language $lang --verify-only true
+
+    tools/generate_document.sh $lang $vers
+
+  fi
+
+done
