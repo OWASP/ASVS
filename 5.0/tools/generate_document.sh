f97d07d37d1236e76cd98c0507c9658425d53edd
Josh Grossman <tghosth@users.noreply.github.com>
2025-09-30 20:11:26 +0300

    Fix doc generation bugs (#3262)
    * Create dir
    
    * Filename fixes

--- 5.0/tools/generate_document.sh
+++ 5.0/tools/generate_document.sh
@@ -13,7 +13,7 @@ if ! command_exists pandoc; then
 fi
 
 generate_docx() {
-    pandoc -s -f gfm --reference-doc=../templates/reference.docx --columns 10000 --toc -t docx -o "../docs_$1/OWASP Application Security Verification Standard $2-$1.docx" *.md
+    pandoc -s -f gfm --reference-doc=../templates/reference.docx --columns 10000 --toc -t docx -o "../docs_$1/OWASP_Application_Security_Verification_Standard_$2_$1.docx" *.md
 	echo " done."
 	echo -e ""
 	echo -e "Note: If you got an error 'Invalid UTF-8 stream', make sure you are on the newest version of pandoc from the project website (not just the OS package repo)"
