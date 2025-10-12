f60beeefa2ae51ee805218757e1375b3ac4597fa
Josh Grossman <tghosth@users.noreply.github.com>
2025-10-12 19:33:07 +0300

    Attempt to run en by default and allow specifying others in a manual run (#3268)

--- docker/run.sh
+++ docker/run.sh
@@ -6,6 +6,12 @@ case $TARGET in
   4.0)
     ./generate-all.sh $LANGS
     ;;
+  5.0)
+    if [ -n "$LANGS" ]; then
+      export SELECTED_LANGS="$LANGS"
+    fi
+    make $FORMATS
+    ;;
   clean)
     make clean
     ;;
